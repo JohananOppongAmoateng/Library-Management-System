d38997e334dfad1c8bf74bb26a459d0624280b7d
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-07-12 17:05:59 +0000

    registered models in admin dashboard

--- LibraryApi/admin.py
+++ LibraryApi/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book,Borrowing,Genre,Author,Publisher,Fine,Reservation
+
+admin.site.register(Book)
+admin.site.register(Borrowing)
+admin.site.register(Publisher)
+admin.site.register(Author)
+admin.site.register(Fine)
+admin.site.register(Reservation)
+admin.site.register(Genre)
+
